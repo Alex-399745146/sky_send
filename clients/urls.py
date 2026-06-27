6c3d89e8c49ad475ea4ae0566bbf39767882d70d
Alexsandr Bachevskiy <bachevskiiaa@gmail.com>
2026-06-27 23:33:47 +0300

    add GRUD message

--- clients/urls.py
+++ clients/urls.py
@@ -4,20 +4,29 @@ from django.urls import path
 
 from .views import (
     home,
-    ClientListView,
-    ClientDetailView,
-    ClientCreateView,
-    ClientUpdateView,
-    ClientDeleteView,
+
+    ClientListView, ClientDetailView, ClientCreateView,
+    ClientUpdateView, ClientDeleteView,
+
+    MessageListView, MessageDetailView, MessageCreateView,
+    MessageUpdateView, MessageDeleteView,
 )
 
 app_name = "clients"
 
 urlpatterns = [
     path("", home, name="home"),
+
     path("clients/", ClientListView.as_view(), name="client_list"),
     path("clients/create/", ClientCreateView.as_view(), name="client_create"),
     path("clients/<int:pk>/", ClientDetailView.as_view(), name="client_detail"),
     path("clients/<int:pk>/edit/", ClientUpdateView.as_view(), name="client_edit"),
     path("clients/<int:pk>/delete/", ClientDeleteView.as_view(), name="client_delete"),
+
+    path("messages/", MessageListView.as_view(), name="message_list"),
+    path("messages/create/", MessageCreateView.as_view(), name="message_create"),
+    path("messages/<int:pk>/", MessageDetailView.as_view(), name="message_detail"),
+    path("messages/<int:pk>/edit/", MessageUpdateView.as_view(), name="message_edit"),
+    path("messages/<int:pk>/delete/", MessageDeleteView.as_view(), name="message_delete"),
+
 ]
--- clients/views.py
+++ clients/views.py
@@ -2,12 +2,15 @@
 from django.shortcuts import render
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.urls import reverse_lazy
-from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.views.generic import (
+    ListView, DetailView, CreateView, UpdateView, DeleteView
+)
 from django.db import models
-from .models import Client, Mailing
+from .models import Client, Mailing, Message
 
 
 def home(request):
+    """Функция передачи инфы в контексте для главной страницы"""
     total_mailings = Mailing.objects.count()
     active_mailings = Mailing.objects.filter(status=Mailing.STATUS_RUNNING).count()
     unique_recipients = Client.objects.count()  # если клиент может быть в нескольких рассылках
@@ -74,3 +77,55 @@ class ClientDeleteView(LoginRequiredMixin, DeleteView):
     model = Client
     template_name = "clients/client_confirm_delete.html"
     success_url = reverse_lazy("clients:client_list")
+
+
+class MessageListView(ListView):
+    """ Чтение всех сообщений """
+    model = Message
+    template_name = "clients/message_list.html"
+    context_object_name = "messages"
+
+    def get_queryset(self):
+        qs = super().get_queryset()
+        q = self.request.GET.get("q")
+        if q:
+            qs = qs.filter(
+                models.Q(subject__icontains=q) |
+                models.Q(body__icontains=q)
+            )
+        return qs
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["q"] = self.request.GET.get("q", "")
+        return context
+
+
+class MessageDetailView(DetailView):
+    """ Чтение конкретного сообщения """
+    model = Message
+    template_name = "clients/message_detail.html"
+    context_object_name = "message"
+
+
+class MessageCreateView(CreateView):
+    """ Создание сообщения(как объект класса) """
+    model = Message
+    fields = ["subject", "body"]
+    template_name = "clients/message_form.html"
+    success_url = reverse_lazy("clients:message_list")
+
+
+class MessageUpdateView(UpdateView):
+    """ Редактирование сообщения """
+    model = Message
+    fields = ["subject", "body"]
+    template_name = "clients/message_form.html"
+    success_url = reverse_lazy("clients:message_list")
+
+
+class MessageDeleteView(DeleteView):
+    """ Удаление сообщения """
+    model = Message
+    template_name = "clients/message_confirm_delete.html"
+    success_url = reverse_lazy("clients:message_list")
